e00a3024becc455d4695f2cd69f1f264d948c47d
Matt <mpschirle1@gmail.com>
2023-02-25 23:26:41 -0800

    Implemented GiftedChat

--- components/Chat.js
+++ components/Chat.js
@@ -1,18 +1,78 @@
 import React from "react";
-import { View, Text, Button, StyleSheet } from "react-native";
+import { View, StyleSheet, Platform, KeyboardAvoidingView } from "react-native";
+import { GiftedChat, Bubble } from "react-native-gifted-chat";
 
 export default class Chat extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      messages: [],
+    };
+  }
+
   componentDidMount() {
     let name = this.props.route.params.name;
     this.props.navigation.setOptions({ title: name });
+
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: "Greetings " + name + "!",
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: "React Native",
+            avatar: "https://placeimg.com/140/140/any",
+          },
+        },
+        // System Message
+        {
+          _id: 2,
+          text: name + " has entered the chat",
+          createdAt: new Date(),
+          system: true,
+        },
+      ],
+    });
+  }
+
+  renderBubble(props) {
+    return (
+      <Bubble
+        {...props}
+        wrapperStyle={{
+          right: {
+            backgroundColor: "#000",
+          },
+        }}
+      />
+    );
+  }
+
+  // Called when user sends a message
+  onSend(messages = []) {
+    this.setState((previousState) => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }));
   }
 
   render() {
     let color = this.props.route.params.color;
-    let name = this.props.route.params.name;
     return (
       <View style={[styles.container, { backgroundColor: color }]}>
-        <Text style={{ textAlign: "center", fontSize: 24 }}>Hello {name}!</Text>
+        <GiftedChat
+          renderBubble={this.renderBubble.bind(this)}
+          messages={this.state.messages}
+          onSend={(messages) => this.onSend(messages)}
+          user={{
+            _id: 1,
+          }}
+        />
+        {/* Input field won’t be hidden beneath the keyboard on Android */}
+        {Platform.OS === "android" ? (
+          <KeyboardAvoidingView behavior="height" />
+        ) : null}
       </View>
     );
   }
